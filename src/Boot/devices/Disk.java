3b0cf401c508dff680956378b560779218d642c0
Null <eizoulee@gmail.com>
2025-07-29 15:52:26 +0800

    Delete src directory

--- src/Boot/devices/Disk.java
+++
@@ -1,9 +0,0 @@
-package Boot.devices;
-import standard.std;
-
-public class Disk extends std{
-    public static void check(){
-        stdout.println("Disk drives checked.");
-        slp(2000);
-    }
-}
