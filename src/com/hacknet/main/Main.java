5b7c8b4ee5e3d45606ef267185a69d13dde77629
Skiawm91 <guoy2674@outlook.com>
2025-10-17 23:30:59 +0800

    我不知道

--- src/com/hacknet/main/Main.java
+++ src/com/hacknet/main/Main.java
@@ -8,7 +8,7 @@ public class Main extends std {
     public static Game game;
 
     public static void main(String[] args) {
-        game = new Game("Hacknet 2024.0208.0 (Copyright (c) AhQiangTW. Do not distribute!)");
+        game = new Game("Hacknet for CMD");
         try {
             game.start();
         } catch (InvalidInputException e) {
